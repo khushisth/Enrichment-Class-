dcebba9456a150e5d72199dfd5ba47d0c99c825c
khushisth <khushishresthahappy@gmail.com>
2025-06-25 14:32:53 +0545

    day 3 learnings

---
+++ day3/login.js
@@ -0,0 +1,31 @@
+//cerate objects like user details
+let details={
+    firstname:"khushi",
+    lastname:"shrestha",
+    email:"khushishrestha@gmail.com",
+    password:"khushi123"
+};
+
+//create function that returns promise, 
+function promiseFunc(email, password){
+    return new Promise(function(resolve, reject){
+        //check and compare the email and password
+        if((email ===details.email)&(password ===details.password)){
+            resolve("You've Sucessfully logged in");
+        } else{
+            reject("Someting went wrong. Try again later.");
+        }
+    })
+};
+
+// async/await checkauthentication function
+async function checkauthentication(){
+    try{
+        const data = await promiseFunc("khushishrestha@gmail.com", "khushi123");
+        console.log(data);
+    } catch(err){
+        console.log(err);
+    }
+}
+
+checkauthentication();
