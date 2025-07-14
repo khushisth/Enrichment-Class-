d3f4d2ba4b69aed93daa2d87401b358a9571ed2a
khushisth <khushishresthahappy@gmail.com>
2025-07-14 22:25:16 +0545

    added

--- nodesetup/routes/leavemgmt.js
+++ nodesetup/routes/leavemgmt.js
@@ -7,3 +7,16 @@ router.post("/login", login);
 
 
 export default router;
+
+import express from 'express'
+import { createEmployee, deleteemployee,  getallemployee,  getemployeeByid,  updateemployee } from '../usercontroller/employeecontroller.js';
+
+const router = express.Router();
+
+router.post('/addemployee',createEmployee)
+router.get('/getemployee',getallemployee)
+router.get('/employee/:employeeid',getemployeeByid)
+router.put('/employee/:employeeid',updateemployee)
+router.delete('/employee/:employeeid',deleteemployee)
+export default router;
+ 
